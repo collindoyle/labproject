d9b033ef0297db678d12abc48ba3215bf6777a65
collindoyle <chuym1983@nii.ac.jp>
2014-04-04 15:12:37 +0900

    Save file

--- XMLConverter/main.cpp
+++ XMLConverter/main.cpp
@@ -19,6 +19,13 @@ int main(int argc, const char * argv[])
 		for (int i = 1; i < argc; i++) {
 			tinyxml2::XMLDocument doc;
 			DrXMLInterpreter::ConvertSource_Takasu(doc, argv[i]);
+			const char * pfilename = strstr(argv[i], ".conv");
+            int len = pfilename - argv[i];
+            char * poutname = new char[len+9];
+            strncpy(poutname, argv[i], len);
+            strcat(poutname, ".conv.xml");
+			doc.SaveFile(poutname);
+            delete [] poutname;
 		}
 
 	}
